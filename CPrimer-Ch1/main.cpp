7ce4275341ed384e407b5be282f5cb5a5acbb578
Seong Kim <mikekim9293@gmail.com>
2016-04-11 22:54:30 -0700

    Initial Commit

---
+++ CPrimer-Ch1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPrimer-Ch1
+//
+//  Created by Seong Kim on 4/11/16.
+//  Copyright © 2016 Seong Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
