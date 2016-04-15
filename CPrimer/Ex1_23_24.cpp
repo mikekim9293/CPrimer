da27ed67806c7741265ec41c35e5c77ca3ff0499
Seong Kim <mikekim9293@gmail.com>
2016-04-14 21:18:09 -0700

    Fix Ex1_23_24.cpp for correct functionality. First commit of Exercises Section 1.6. Chapter 1 finished.

--- CPrimer/Ex1_23_24.cpp
+++ CPrimer/Ex1_23_24.cpp
@@ -12,17 +12,19 @@
 
 int main() {
     Sales_item book, book_curr;
+    int count = 1;
     std::cout << "Input book transactions: " << std::endl;
     if (std::cin >> book_curr) {
         while (std::cin >> book)
             if (book.isbn() == book_curr.isbn())
-                book_curr += book;
+                ++count;
             else {
-                std::cout << book_curr << std::endl;
+                std::cout << count << " transactions for " << book_curr.isbn() << std::endl;
                 book_curr = book;
+                count = 1;
             }
         }
-    std::cout << book_curr << std::endl;
+    std::cout << count << " transactions for " << book_curr.isbn() << std::endl;
     return 0;
     // Output:
     // Input book transactions:
@@ -34,7 +36,7 @@ int main() {
     // 0-259-18149-X 2 125.00
     // 0-259-18149-X 3 99.00
     // 0-259-18149-X 6 70.00
-    // 0-201-78345-X 8 174 21.75
-    // 0-259-18149-X 12 1047 87.25
-    Program ended with exit code: 0
+    // 4 transactions for 0-201-78345-X
+    // 4 transactions for 0-259-18149-X
+    // Program ended with exit code: 0
 }
