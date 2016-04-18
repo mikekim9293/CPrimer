f49e42ce6047c48eeba5b7b3cbe01beab0831eae
Seong Kim <mikekim9293@gmail.com>
2016-04-17 20:56:59 -0700

    First commit of Exercises Section 2.2.3.

---
+++ CPrimer/Ch2/Ex2_12.cpp
@@ -0,0 +1,29 @@
+//
+//  Ex2_12.cpp
+//  CPrimer
+//
+//  Created by Seong Kim on 4/17/16.
+//  Copyright © 2016 Seong Kim. All rights reserved.
+//
+
+// Exercise 2.12 (below)
+#include <iostream>
+
+int main() {
+    return 0;
+    // Output:
+    // Program ended with exit code: 0
+}
+
+// Exercise 2.12
+/* (a) int double = 3.14;
+ * Invalid: C++ keyword
+ * (b) int _;
+ * Valid
+ * (c) int catch-22;
+ * Invalid: because of -
+ * (d) int 1_or_2 = 1;
+ * Invalid: begins with a digit, not a letter or underscore
+ * (e) double Double = 3.14;
+ * Valid
+ */
