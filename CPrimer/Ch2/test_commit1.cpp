c50fc6a7fd243cd96383d05c1cfc75c245279a65
Seong Kim <mikekim9293@gmail.com>
2016-04-21 09:02:20 -0700

    Test commit 1. Source file will be deleted by 22 April 2016.

---
+++ CPrimer/Ch2/test_commit1.cpp
@@ -0,0 +1,15 @@
+//
+//  test_commit1.cpp
+//  CPrimer
+//
+//  Created by Seong Kim on 4/21/16.
+//  Copyright © 2016 Seong Kim. All rights reserved.
+//
+
+// test_commit1
+// Will be deleted by 22 April 2016.
+#include <iostream>
+
+int main() {
+    return 0;
+}
