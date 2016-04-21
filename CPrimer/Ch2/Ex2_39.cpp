f8567b3da9f16be50d9e35f2b3d11b4d76712f04
Seong Kim <mikekim9293@gmail.com>
2016-04-20 21:44:13 -0700

    First commit of Exercise Section 2.6.1.

---
+++ CPrimer/Ch2/Ex2_39.cpp
@@ -0,0 +1,21 @@
+//
+//  Ex2_39.cpp
+//  CPrimer
+//
+//  Created by Seong Kim on 4/20/16.
+//  Copyright © 2016 Seong Kim. All rights reserved.
+//
+
+// Exercise 2.39
+#include <iostream>
+
+struct Foo { /* empty */ };
+
+int main() {
+    return 0;
+    // Output:
+    // Program ended with exit code: 0
+}
+
+// Error:
+// Expected ';' after struct
